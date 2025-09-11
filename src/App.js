5f527ee949f6bae82c6f14751d59acdc36b47ca1
TRAGIC11 <piyush.bhujbal0201@gmail.com>
2025-09-11 06:11:53 +0530

    change uri

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from "./components/HomePage";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/Portfolio">
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
